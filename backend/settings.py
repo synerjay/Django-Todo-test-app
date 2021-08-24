ca55b15bd80667818c13d190f5c84059f1a87605
Jerome Tolentino <tolent.jay@gmail.com>
2021-08-24 23:35:32 +0900

    Third try

--- backend/settings.py
+++ backend/settings.py
@@ -11,15 +11,14 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import environ 
 
 import dotenv
 import dj_database_url
 import os
 
+dotenv.load_dotenv()
+
 # Initialise environment variables
-env = environ.Env()
-environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -33,7 +32,7 @@ if os.path.isfile(dotenv_file):
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
